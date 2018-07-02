16032f77eafa01286cf1d8bf0574773129abf7a5
Eduardo Fernandez <efernandez@sweetspot.com>
2018-07-02 20:45:51 +0200

    Set foundation of game

---
+++ chapter 6/dungeon_crawl/lib/dungeon_crawl/cli/main.ex
@@ -0,0 +1,21 @@
+defmodule DungeonCrawl.CLI.Main do
+
+  alias Mix.Shell.IO, as: Shell
+
+  def start_game do
+    welcome_message()
+    Shell.prompt("Please enter to continue")
+    hero_choice()
+  end
+
+  defp welcome_message do
+    Shell.info("== Dungeon Crawl ==")
+    Shell.info("You awake in a dungeon full of monsters.")
+    Shell.info("You need to survive and find the exit.")
+  end
+
+  defp hero_choice do
+    DungeonCrawl.CLI.HeroeChoice.start()
+  end
+
+end
